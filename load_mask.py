8a193e48c971a0de02265a5bffe9854f375fb094
Matthew Turk <matthewturk@gmail.com>
2024-05-21 13:40:58 -0500

    works

--- load_mask.py
+++ load_mask.py
@@ -7,20 +7,22 @@ from xml.etree import ElementTree as ET
 bounds = (390, 250, 203, 242)
 
 im = np.zeros((bounds[2], bounds[3]))
+im_flat = im.T.flat
 
 tree = ET.parse("volume_data.xml")
 for ad in tree.findall(".//AreaData"):
     buf = base64.b64decode(ad.text)
     l = dotnetlist.Dotnetlist.from_bytes(buf)
-    arr = np.array(l.records[2].record.values).reshape((-1, 2), order='F')
-    arr = arr[arr[:,0] > 0, :]
-    for pair in arr:
-        im[pair[1], pair[0]] = 1
+    a1 = np.array(l.records[2].record.values)
+    pos = 0
+    on = 1
+    for a in a1:
+        im_flat[pos: pos + a] = on
+        pos += a
+        on = ~on
     plt.imshow(im)
     plt.savefig("hi.png")
     plt.clf()
-    plt.plot(arr[:,0], arr[:,1])
-    plt.savefig("hi2.png")
 
 history = base64.b64decode(tree.find(".//History").text)
 setting = dotnetlist.Dotnetlist.from_bytes(base64.b64decode(tree.find(".//Setting").text))
