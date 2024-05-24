023daf9a6eb29e3a3e1645926c6efe064a98ee2c
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2024-05-24 10:36:15 -0500

    Overplot mask on light image

--- load_mask.py
+++ load_mask.py
@@ -1,14 +1,23 @@
+import matplotlib
+
+matplotlib.use("Qt5Agg")
 import numpy as np
 import base64
 import dotnetlist
 import matplotlib.pyplot as plt
 from xml.etree import ElementTree as ET
+from vk4 import Vk4
+
+dataset = Vk4.from_file("Vk4File")
+
+data_image = dataset.color_light
+v = np.frombuffer(data_image.data, dtype="uint8")
+v.shape = (data_image.height, data_image.width, 3)
+
 
 bounds = (390, 250, 203, 242)
-
 im = np.zeros((bounds[2], bounds[3]))
 im_flat = im.T.flat
-
 tree = ET.parse("volume_data.xml")
 for ad in tree.findall(".//AreaData"):
     buf = base64.b64decode(ad.text)
@@ -17,12 +26,25 @@ for ad in tree.findall(".//AreaData"):
     pos = 0
     on = 1
     for a in a1:
-        im_flat[pos: pos + a] = on
+        im_flat[pos : pos + a] = on
         pos += a
         on = ~on
-    plt.imshow(im)
-    plt.savefig("hi.png")
-    plt.clf()
+
+fig = plt.figure(frameon=False)
+extent = (0, data_image.width, 0, data_image.height)
+im1 = plt.imshow(v, extent=extent)
+
+mask = np.zeros((data_image.width, data_image.height))
+mask[bounds[0] : bounds[0] + bounds[2], bounds[1] : bounds[1] + bounds[3]] = im
+im2 = plt.imshow(
+    mask.T,
+    extent=extent,
+    alpha=0.2,
+)
 
 history = base64.b64decode(tree.find(".//History").text)
-setting = dotnetlist.Dotnetlist.from_bytes(base64.b64decode(tree.find(".//Setting").text))
+setting = dotnetlist.Dotnetlist.from_bytes(
+    base64.b64decode(tree.find(".//Setting").text)
+)
+
+plt.show()
